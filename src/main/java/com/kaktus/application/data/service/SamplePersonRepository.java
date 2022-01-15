9d4541c32196ba473ec2207eecd4cd764a356790
Milan Danilovic <mdanilovic06@gmail.com>
2022-01-15 23:40:32 +0100

    Final

--- src/main/java/com/kaktus/application/data/service/SamplePersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kaktus.application.data.service;
-
-import com.kaktus.application.data.entity.SamplePerson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, Integer> {
-
-}
